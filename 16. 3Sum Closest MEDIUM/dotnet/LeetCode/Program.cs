5e1746419d93d847eba762feff730a6008769527
Sultonxon <qudratovsultonxon20011124@gmaIL.COM>
2025-08-28 19:35:30 +0500

    solved 16

---
+++ 16. 3Sum Closest MEDIUM/dotnet/LeetCode/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCode;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(new Solution().ThreeSumClosest([0,1,2], 0));
+
---
+++ 16. 3Sum Closest MEDIUM/dotnet/LeetCode/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode;
+
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target) {
+        
+        Array.Sort(nums);
+        var sum = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if(i>0 && nums[i-1] == nums[i]) continue;
+            
+            var left = i + 1;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var currentSum = nums[i] + nums[left] + nums[right];
+                var currentDiff = currentSum - target;
+                var diff = Math.Abs(sum - target);
+
+                if (Math.Abs(currentDiff) < diff)
+                {
+                    sum = currentSum;
+                }
+
+                if (currentDiff > 0)
+                {
+                    right--;
+                }
+                else if (currentDiff < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    return currentSum;
+                }
+                
+            }
+        }
+
+        return sum;
+    }
+    
+}
