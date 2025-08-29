beecd5da623cb95b12e09969d316c617ce0d8de9
Sultonxon <qudratovsultonxon20011124@gmaIL.COM>
2025-08-29 19:41:30 +0500

    solved 31

---
+++ 31. Next Permutation/dotnet/LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;Console.WriteLine("Hello, World!");
+
+Random rand = new Random();
+int[] nums = Enumerable.Range(0, 99).OrderBy(x => rand.Next()).ToArray();
+
+
+int[] arr = [2,3,1];
+var start = DateTime.Now;
+new Solution().NextPermutation(nums);
+Console.WriteLine(DateTime.Now - start);
+Console.WriteLine(string.Join(", ", nums));
---
+++ 31. Next Permutation/dotnet/LeetCode/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode;
+
+public class Solution
+{
+    public void NextPermutation(int[] nums)
+    {
+        int point = -1;
+
+        for (int i = nums.Length - 2; i >= 0 ; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                point = i;
+                break;
+            }
+        }
+        if (point == -1)
+        {
+            Array.Sort(nums);
+            return;
+        }
+        var pointValue = nums[point];
+        var replacementIndex = point + 1;
+        var replacementValue = nums[replacementIndex];
+        for (int i = nums.Length - 1 ; i > point; i--)
+        {
+            if (pointValue < nums[i])
+            {
+                replacementIndex = i;
+                replacementValue = nums[i];
+                break;
+            }
+        }
+        
+        nums[point] = nums[replacementIndex];
+        nums[replacementIndex] = pointValue;
+        Array.Sort(nums, point + 1, nums.Length - point - 1);
+    }
+}
