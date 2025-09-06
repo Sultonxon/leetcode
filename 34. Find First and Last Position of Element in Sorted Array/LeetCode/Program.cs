9dff8558e387d4db7e974c62aa805ef490e6bb1b
Sultonxon <qudratovsultonxon20011124@gmaIL.COM>
2025-09-06 15:01:38 +0500

    s

---
+++ 34. Find First and Last Position of Element in Sorted Array/LeetCode/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Net.Sockets;
+using LeetCode;
+
+Console.WriteLine(new Solution().SearchRange([0,1,1,1,2,2,4,5,5,5,6,6,7,7,7,7,7,8,9,9,10], 5));
---
+++ 34. Find First and Last Position of Element in Sorted Array/LeetCode/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode;
+
+public class Solution
+{
+
+    public int[] SearchRange(int[] nums, int target)
+    {
+        var left = 0;
+        while (true)
+        {
+            if (nums[left] < target) left = (left + nums.Length - 1) / 2;
+            else if (nums[left] > target) left = left / 2;
+            else
+            {
+                if (left == 0 || nums[left - 1] < target) break;
+                else left = left / 2;
+            }
+        }
+
+        var right = nums.Length - 1;
+
+        while (true)
+        {
+            if (nums[right] > target) right = (right + left) / 2;
+            else if(nums[right] < target) right = (right + nums.Length - 1) / 2;
+            else
+            {
+                if(right == nums.Length - 1 || nums[right + 1] > target) break;
+                else right = (right + left) / 2;
+            }
+        }
+        
+        return new int[] { left, right };
+    }
+}
