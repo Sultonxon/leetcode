d3b829195f055a9d460358d092962477b898942d
Sultonxon <qudratovsultonxon20011124@gmaIL.COM>
2025-09-06 12:34:25 +0500

    solved

--- 33. Search in Rotated Sorted Array/dotnet/LeetCode/Program.cs
+++ 33. Search in Rotated Sorted Array/dotnet/LeetCode/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using LeetCode;
+
+Console.WriteLine(new Solution().Search([4,5,6,7,0,1,2], 0));
+
--- 33. Search in Rotated Sorted Array/dotnet/LeetCode/Solution.cs
+++ 33. Search in Rotated Sorted Array/dotnet/LeetCode/Solution.cs
@@ -4,28 +4,19 @@ public class Solution
 {
     public int Search(int[] nums, int target)
     {
-        var start = 0;
-        for (int i = 0; i < nums.Length - 1; i++)
-        {
-            if (nums[i] > nums[i + 1])
-            {
-                start = i + 1;
-                break;
-            }
-        }
 
         int point = 0;
         int left = 0;
         int right = nums.Length - 1;
-        while (true)
+        while (left < right)
         {
-            if (nums[left] < nums[right])
+            if (nums[left] > nums[right])
             {
-                left = (right + left) / 2;
+                left++;
             }
-            else if (nums[left] > nums[right])
+            else if (nums[left] < nums[right])
             {
-                right = (left + right) / 2;
+                right--;
             }
 
             if (left == right - 1)
@@ -38,11 +29,32 @@ public class Solution
 
         int rotation = point;
 
-        while (true)
+        int l = 0;
+        int r = nums.Length - 1;
+        while (r - l > 1)
         {
-            
+            var rotatedL = GetRotatedIndex(l, rotation, nums);
+            var rotatedR = GetRotatedIndex(r, rotation, nums);
+            var mid = (l + r) / 2;
+            var rotatedMid = GetRotatedIndex(mid, rotation, nums);
+            if (nums[rotatedMid] == target) return rotatedMid;
+            else if (nums[rotatedMid] > target) r = mid;
+            else l = mid;
         }
-        
+
+
+        if(nums[GetRotatedIndex(l, rotation, nums)] == target) return GetRotatedIndex(l, rotation, nums);
+        if(nums[GetRotatedIndex(r, rotation, nums)] == target) return GetRotatedIndex(r, rotation, nums);
+
+        return -1;
+
+    }
+
+    private int GetRotatedIndex(int actualIndex, int rotation, int[] array)
+    {
+        var length = array.Length;
+        var rotatedIndex = actualIndex + rotation;
+        return rotatedIndex >= length ? rotatedIndex - length : rotatedIndex;
     }
     
 }
